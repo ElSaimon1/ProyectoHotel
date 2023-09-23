60f407b48ffdabbe50cf40f2d142bf8881dfaa09
simon ruz <sim.ruz@duocuc.cl>
2023-09-23 19:10:02 -0300

    Cree la clase libro

---
+++ JavaApplication5/src/Libro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ridox
+ */
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int precio;
+    private String isbn;
+    
+}
