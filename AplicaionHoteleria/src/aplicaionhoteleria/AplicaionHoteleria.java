3bcedee7ed6e3f5f129b1682aa447d58ce32ae47
simon ruz <sim.ruz@duocuc.cl>
2023-11-09 17:30:02 -0300

    Proyecto Hoteleria

---
+++ AplicaionHoteleria/src/aplicaionhoteleria/AplicaionHoteleria.java
@@ -0,0 +1,13 @@
+package aplicaionhoteleria;
+/**
+ *
+ * @author saimon
+ */
+public class AplicaionHoteleria {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
